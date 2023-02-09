7ad6d9089c04a5b5061b93a9d3a0091981eb28ae
TomasAleman <alemanmdq@gmail.com>
2023-02-09 16:14:36 -0300

    validations and style

--- main.js
+++ main.js
@@ -17,10 +17,14 @@ close.addEventListener("click", (e) => {
 
 add.addEventListener("click", (e) => {
     e.preventDefault();
-    document.sample.name.value = "";
-    document.sample.price.value = "";
-    document.sample.description.value = "";
-    document.sample.fork.value = "";
+
+    var simbol = document.sample.simbol.value;
+    var name = document.sample.name.value;
+    var price = document.sample.price.value;
+    var description = document.sample.description.value;
+    var fork = document.sample.fork.value;
+
+    addRow(name, simbol, price, description, fork);
 
     var tab = document.getElementById("tbl");
     var rowCount = tab.childElementCount;
@@ -31,17 +35,21 @@ add.addEventListener("click", (e) => {
     } else {
         tableEl.style.display = "block";
         empty.style.display = "none"
-        document.querySelector(".add-menu").style.display = "none";
+        if ((validatePrice(price) && validateForks(fork)) && name != "" && price != "" && fork != "") {
+            document.querySelector(".add-menu").style.display = "none";
+            document.sample.name.value = "";
+            document.sample.price.value = "";
+            document.sample.description.value = "";
+            document.sample.fork.value = "";
+        } else {
+            document.querySelector(".add-menu").style.display = "flex";
+        }
     }
 });
 
-function addRow() {
-    var name = document.sample.name.value;
-    var price = "$" + document.sample.price.value;
-    var description = document.sample.description.value;
-    var fork = document.sample.fork.value;
+function addRow(name, simbol, price, description, fork) {
 
-    if (name != "" && price != "$" && fork != "") {
+    if ((validatePrice(price) && validateForks(fork)) && name != "" && price != "" && fork != "") {
         var tr = document.createElement('tr');
 
         var td1 = tr.appendChild(document.createElement('td'));
@@ -51,16 +59,17 @@ function addRow() {
         var td5 = tr.appendChild(document.createElement('td'));
 
         td1.innerHTML = name;
-        td2.innerHTML = price;
+        td2.innerHTML = simbol + price;
         td3.innerHTML = description;
         td4.innerHTML = fork;
         td5.innerHTML = '<input type="button" name="remove" value="Remove" onclick="remRow(this);" class="deletebtn">';
 
         document.getElementById("tbl").appendChild(tr);
-    }else{
-        alert("Please complete the fields");
-    }
-    valCountRows();
+    } else if (!validatePrice(price) || !validateForks(fork)) {
+        alert("Insert a valid value"); 
+    } else {
+        alert("Please complete all the fields");  
+    }    
 };
 
 function remRow(row) {
@@ -80,4 +89,18 @@ function valCountRows() {
         tableEl.style.display = "block";
         empty.style.display = "none"
     }
+}
+
+function validatePrice(price) {
+    if (price < 0)
+        return false;
+    else
+        return true;
+}
+
+function validateForks(fork) {
+    if (fork < 0)
+        return false;
+    else
+        return true;
 }
